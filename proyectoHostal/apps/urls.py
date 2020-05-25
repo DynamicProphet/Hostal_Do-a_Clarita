2a93e8020674abaff010297a70cd5e153b8c7743
Emilio Rojas <erojas@arkho.tech>
2020-05-24 23:34:18 -0400

    Implementación de login de usuarios y otros ajustes

--- proyectoHostal/apps/urls.py
+++ proyectoHostal/apps/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url, include
+#from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 #Para importar el login_required y aplicarlo en el Crud
 from django.contrib.auth.decorators import login_required
@@ -13,5 +14,5 @@ from rest_framework.urlpatterns import format_suffix_patterns
 #from .views import ProductViewSet, ProductViewSetDetail
 
 urlpatterns = [
-    url(r'^', home, name='pagina_principal'),
+    path('', home, name='pagina_principal'),
 ]
